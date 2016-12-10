d76485363167916faaf5146544e0b6369766a063
jelonicolejavier <jelo@Josephs-MacBook-Air.local>
2016-12-11 05:54:17 +0800

    fix paeng's mess

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,18 +1,27 @@
   <!DOCTYPE html>
   <html lang="en">
     <head>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="{!! URL::asset('../js/jquery-2.1.1.min.js') !!}"></script>
+      <script type="text/javascript" src="{!! URL::asset('../js/materialize.min.js') !!}"></script>
+      <script type="text/javascript" src="{!! URL::asset('../js/prism.js') !!}"></script>
+      <script type="text/javascript" src="{!! URL::asset('../js/custom-min.js') !!}"></script>
+      <script type="text/javascript" src="{!! URL::asset('../js/plugin-min.js') !!}"></script>
+      <script type="text/javascript" src="{!! URL::asset('../js/perfect-scrollbar.min.js') !!}"></script>
+      <script type="text/javascript" src="{!! URL::asset('../js/jquery.magnific-popup.min.js') !!}"></script>
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link rel="stylesheet" type="text/css" href="css/prism.css">
-      <link rel="stylesheet" type="text/css" href="css/custom-min.css">
-      <link rel="stylesheet" type="text/css" href="css/plugin-min.css">
-      <link rel="stylesheet" type="text/css" href="css/perfect-scrollbar.css">
-      <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
-      <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"> 
-      <link rel="stylesheet" type="text/css" href="css/hover.min.css">
-      <link rel="stylesheet" type="text/css" href="styles.css">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/materialize.min.css') !!}" media="screen,projection">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/prism.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/custom-min.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/plugin-min.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/perfect-scrollbar.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/magnific-popup.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/font-awesome.min.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/hover-min.css') !!}">
+      <link rel="stylesheet" type="text/css" href="{!! URL::asset('../css/jquery.dataTables.min.css') !!}">
+
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -44,11 +53,11 @@
                   <div class="col s12">
                     <form action="#">
                         <p>
-                          <input name="group1" type="radio" id="test1" />
+                          <input name="group1" type="radio" id="test1" class = "with-gap"/>
                           <label for="test1">Male</label>
                         </p>
                         <p>
-                          <input name="group1" type="radio" id="test2" />
+                          <input name="group1" type="radio" id="test2" class = "with-gap"/>
                           <label for="test2">Female</label>
                         </p>
                       </form>
@@ -102,6 +111,9 @@
 
     @section('scripts')
 
+      <script type="text/javascript" src = "{!! URL::asset('../js/process/registration.js') !!}"></script>
+
+
       <script type="text/javascript">
         function readURL(input) {
           if (input.files && input.files[0]) {
@@ -116,16 +128,11 @@
 
               reader.readAsDataURL(input.files[0]);
           }
-      }
+        }
       </script>
     @endsection
       <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-      <script type="text/javascript" src="js/prism.js"></script>
-      <script type="text/javascript" src="js/custom-min.js"></script>
-      <script type="text/javascript" src="js/plugin-min.js"></script>
-      <script type="text/javascript" src="js/perfect-scrollbar.js"></script>
-      <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+
+      
     </body>
   </html>
